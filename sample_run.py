e400851c11a9a661d78e2725146157dfd86fe728
Bhavarth Pandya <bhavarth@ssid-umass-vl924-172-30-26-205.wireless.umass.edu>
2017-12-17 19:42:43 -0500

    add more usage instructions

--- sample_run.py
+++ sample_run.py
@@ -12,10 +12,13 @@ im = cv2.imread("sample_symbol.png",0)
 # models : 'svm', 'rf'
 # func : 'test', 'train', 'validate'
 sclf = symbolClassifier(model='svm',func='test')
-# pass image as 2d numpy array to predict_class() method
+# pass image as 2d numpy array to predict_class() method, it will take care of resizing it to the training sample size and feature extraction.
 # returns number label, name label and probability of the label
 label, name, prob = sclf.predict_class(im)
 # to get other probabilities : sclf.probabilities stores an array of all class probabilities for test image
-
+#
+# to get cross-validation scores : use func='validate' it first performs a stratified k-fold cross validation and store the accuracy score in sclf.cval_score,
+# then trains the model on full training data storing the model in sclf.clf
+#
 print("Class : ", label,name)
 print("Probability : ", prob)
